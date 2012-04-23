3bf0996074c77ea55f866ca6d298d5ffabad4e0f
Andris Reinman <andris@node.ee>
2012-04-23 15:33:02 +0300

    update

--- tools/clientplayground.js
+++ tools/clientplayground.js
@@ -6,17 +6,18 @@ var gmail = {
         user: "test.nodemailer@gmail.com",
         pass: "Nodemailer123"
     },
-    debug: false
+    debug: true
 };
 
-var imap = new IMAPClient(false, "imap.gmail.com", gmail);
+var imap = new IMAPClient(false, "imap.gmail.com", gmail);
 
 imap.on("error", console.log);
 
 imap.on("mailbox", function(mailbox){
     console.log("INBOX")
     console.log(mailbox)
-    imap.close();
+    console.log(imap.getMailboxList());
+    imap.idle();
 });
 
 
