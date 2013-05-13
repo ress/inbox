d701910b067fd2f24b5d9827e2aea53c3adeceae
Andris Reinman <andris@kreata.ee>
2013-05-13 14:51:28 +0300

    fixed example

--- examples/list.js
+++ examples/list.js
@@ -1,6 +1,6 @@
 var inbox = require(".."),
     util = require("util");
-    
+
 var client = inbox.createConnection(false, "imap.gmail.com", {
     secureConnection: true,
     auth:{
@@ -13,12 +13,12 @@ var client = inbox.createConnection(false, "imap.gmail.com", {
 client.connect();
 
 client.on("connect", function(){
-    
+
     client.listMailboxes(console.log);
-    
+
     client.openMailbox("INBOX", function(error, mailbox){
         if(error) throw error;
-        
+
         // List newest 10 messages
         client.listMessages(-10, function(err, messages){
             messages.forEach(function(message){
@@ -33,17 +33,16 @@ client.on("connect", function(){
             });
         });
     });
-    
+
     // on new messages, print to console
     client.on("new", function(message){
         console.log("New message:");
         console.log(util.inspect(message, false, 7));
-        
+
         client.createMessageStream(message.UID).pipe(process.stdout, {end: false});
+    });
 
     client.on('close', function (){
         console.log('DISCONNECTED!');
-    });
-        
     });
 });
